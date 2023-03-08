b94632481118ddbf926e9fae49b595754275fcdf
jk-Nam <skawnsrn94@gmail.com>
2023-03-08 13:22:42 +0900

    크로스헤어 색상 변경

--- Assets/2.Scripts/UIMgr.cs
+++ Assets/2.Scripts/UIMgr.cs
@@ -10,10 +10,14 @@ public class UIMgr : MonoBehaviour
     public  GameObject go_OptionUI;
 
     public GameObject[] crosshair;
+    public Color[] crosshairColor;
+
+    private int currnetColorIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -69,6 +73,28 @@ public class UIMgr : MonoBehaviour
         for (int i = 0; i < crosshair.Length; i++)
         {
             crosshair[i].gameObject.SetActive(i == index);
+        }
+    }
+
+    public void CrosshairColorChange(int index)
+    {
+        if (index >= 0 && index < crosshairColor.Length)
+        {
+            currnetColorIndex = index;
+            SetCrosshairColor(crosshairColor[currnetColorIndex]);
+        }
+    }
+
+    private void SetCrosshairColor(Color color)
+    {
+        for (int i = 0; i <crosshair.Length; i++)
+        {
+            Image[] images = crosshair[i].GetComponentsInChildren<Image>();
+            foreach (Image image in images)
+            {
+                //color.a = 1.0f;
+                image.color = color;
+            }
         }
     }
 }
